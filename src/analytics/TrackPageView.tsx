161e9bb4a82dad49745d191d400390055c9efac4
sanskaryo <sanskartodwal@gmail.com>
2026-02-01 23:04:55 +0530

    added analytics direct

--- src/analytics/TrackPageView.tsx
+++ src/analytics/TrackPageView.tsx
@@ -1,15 +1,21 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import ReactGA from "react-ga4";
+
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
 
 export default function TrackPageView() {
   const location = useLocation();
 
   useEffect(() => {
-    ReactGA.send({
-      hitType: "pageview",
-      page: location.pathname + location.search,
-    });
+    if (window.gtag) {
+      window.gtag("config", "G-271J2BC4BM", {
+        page_path: location.pathname + location.search,
+      });
+    }
   }, [location]);
 
   return null;
