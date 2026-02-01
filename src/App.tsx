975e4832c2cadf1794cd2da65360fbf74612d4d5
sanskaryo <sanskartodwal@gmail.com>
2026-02-01 22:00:30 +0530

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import TrackPageView from "./analytics/TrackPageView";
 
 const queryClient = new QueryClient();
 
@@ -13,13 +14,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+
+      <BrowserRouter basename="/Sanskar-portfolio">
+        <TrackPageView />
+
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-271J2BC4BM");
+ReactGA.send("pageview");
 
 createRoot(document.getElementById("root")!).render(<App />);
