9c47537b8defcb29b2368888d0129070c00e0f5f
sanskaryo <sanskartodwal@gmail.com>
2026-02-01 23:09:51 +0530

    remove react-ga4 and finalize gtag analytic

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import ReactGA from "react-ga4";
-
-ReactGA.initialize("G-271J2BC4BM");
-ReactGA.send("pageview");
 
 createRoot(document.getElementById("root")!).render(<App />);
